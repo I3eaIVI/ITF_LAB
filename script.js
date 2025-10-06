54607dd2b68d7be916ec5e10557c580aaaeb9bbb
I3eaIVI <i3eaivi6906@gmail.com>
2025-10-06 18:01:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const my_input = document.getElementById("my_input")
-let current_value = 1
+let historyBox = document.getElementById("history");
+let accountInput = document.getElementById("accountBalance");
+let cashInput = document.getElementById("cashBalance");
+let amountInput = document.getElementById("amount");
 
-function calc(x) {
-	current_value += Number(x) // Calculate Variable `current_value`
-	my_input.value = current_value // Set my_input Value to `current_value`
+function updateBalances() {
+  historyBox.value += `\n> Balances updated: Account = ${accountInput.value}, Cash = ${cashInput.value}`;
+}
+
+function deposit() {
+  let amount = parseFloat(amountInput.value);
+  let cash = parseFloat(cashInput.value);
+  let account = parseFloat(accountInput.value);
+
+  if (cash >= amount) {
+    account += amount;
+    cash -= amount;
+
+    accountInput.value = account;
+    cashInput.value = cash;
+
+    historyBox.value += `\n> Deposited ${amount}`;
+  } else {
+    historyBox.value += `\n> Deposit failed: Not enough cash`;
+  }
+}
+
+function withdraw() {
+  let amount = parseFloat(amountInput.value);
+  let cash = parseFloat(cashInput.value);
+  let account = parseFloat(accountInput.value);
+
+  if (account >= amount) {
+    account -= amount;
+    cash += amount;
+
+    accountInput.value = account;
+    cashInput.value = cash;
+
+    historyBox.value += `\n> Withdrew ${amount}`;
+  } else {
+    historyBox.value += `\n> Withdrawal failed: Not enough account balance`;
+  }
 }
